dbbeb2591ee24f86e925f489d929ed5e744bfcd8
filip-tod <filip.tod95@gmail.com>
2023-05-08 11:13:10 +0200

    viježba 2

---
+++ vijezba2/2. Numbers - Please start here/code.js
@@ -0,0 +1,92 @@
+var x = 1;
+console.log(typeof x);
+
+//fractional numbers
+var y = 2.4;
+
+var z = 5.3e8;
+console.log(z);
+
+//max min in JS
+
+var max = Number.MAX_VALUE
+console.log(max);
+
+var min=Number.MIN_VALUE
+console.log(min);
+
+//arithmetic operation 
+var a = 4 + 9;
+console.log(a);
+
+a = 3 * 4;
+console.log(a);
+
+a = y- x;
+console.log(a);
+
+var b = 3;
+//b = b + 2;
+b += 2;
+console.log(b);
+b += 1;
+console.log(b);
+b ++;
+console.log(b);
+
+b -= 1
+console.log(b);
+b--;
+console.log(b);
+
+//operator precedence
+var c = 4 * (5 + 3);
+console.log(c);
+
+var d = ++ c ;
+console.log(c);
+console.log(d);
+
+var e = c ++
+console.log(e);
+console.log(c);
+
+//ostatak djeljenja
+
+var f = 17 % 3;
+console.log(f);
+
+
+//exponent operator
+var g = 3 ** 3 ; // 3 na četvrtu
+console.log(g);
+
+// infinity
+var h = Infinity;
+console.log(h);
+
+//NaN
+var i  = 0/0; //nema smisla (not  a number)
+console.log(i);
+
+//power methode
+var j  = Math.pow(2, 53);
+console.log(j);
+
+//big number 
+var k = Math.pow(2, 2000);
+console.log(k);
+
+//parse a string and return an intiger
+var num = '3';
+console.log(typeof 
+    parseInt(num));
+
+    console.log( 
+        parseInt('nije broj'));
+        console.log(typeof 
+            parseInt('nije broj'));
+
+ 
+
+    
