c6797defa659a9ac53de3a33f55764ae26b07fb8
filip-tod <filip.tod95@gmail.com>
2023-05-08 10:12:45 +0200

    first commit

---
+++ vijezba1/code.js
@@ -0,0 +1,7 @@
+//show  a alert massage say Hi
+
+window.alert('hi');
+
+// print something in console
+
+console.log('print nesto');
---
+++ vijezba2/1. Variables - Please start here/code.js
@@ -0,0 +1,15 @@
+// deklaracija varijabli 
+var score ;
+console.log(score);
+
+
+// deklaracija i inicijalizacija
+var LivesLeft = 3 ;
+console.log(LivesLeft);
+
+//re-assing a value to our variable
+LivesLeft = 2;
+console.log(LivesLeft);
+
+//konstante const
+const firstName = 'John';
